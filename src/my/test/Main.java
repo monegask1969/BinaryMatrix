a791a9adad9f6e3ca13d7dd48fbd846fb21b6e0d
moneg <monegask1969@gmail.com>
2019-04-04 10:25:48 +0300

    dif classes

--- src/my/test/Main.java
+++ src/my/test/Main.java
@@ -1,15 +1,50 @@
 package my.test;
 
-import static my.test.MatrixFunc.rowWithMax1s;
 
 public class Main {
-    
-    public static void main(String[] args)
-    {
-        int mat[][] = { { 0, 0, 0, 1, 0, 0},
-                        { 0, 0, 1, 1, 0, 0},
-                        { 0, 0, 0, 1, 0, 0}};
-        System.out.println("Index of row with maximum 1s is "
-                + rowWithMax1s(mat));
+
+    public static void main(String[] args) {
+        int mat[][] = {{0, 0, 0, 1, 0, 0},
+                       {0, 0, 1, 1, 0, 0},
+                       {0, 0, 0, 1, 0, 0}};
+
+        int maxRow = 0;
+        int maxColumn = 0;
+        int row = 0, column=0;
+        int resRow = 0;
+        int resColumn = 0;
+        for (int i = 0; i < 3; i++) {
+            maxRow = 0;
+            for (int j = 0; j < 6; j++) {
+                if (mat[i][j] == 1) {
+                    maxRow += 1;
+                }
+                if (maxRow > resRow) {
+                    resRow = maxRow;
+                    row = i;
+                }
+            }
+
+        }
+        System.out.println("ROUND");
+        for (int i = 0; i < 6; i++) {
+            maxColumn = 0;
+            for (int j = 0; j < 3; j++) {
+                if (mat[j][i] == 1) {
+                    maxColumn += 1;
+                }
+                if (maxColumn > resColumn) {
+                    resColumn = maxColumn;
+                    column = i;
+                }
+            }
+
+        }
+
+        System.out.println("Maximum 1's is " + ((resColumn > resRow) ? resColumn:resRow));
+
+        System.out.println("Row, that has max 1's: " + row);
+        System.out.println("Column, that has max 1's: " + column);
+
     }
 }
