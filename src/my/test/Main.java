fab077c42c2f6ae1aaa412ede4fa904459997927
moneg <monegask1969@gmail.com>
2019-04-04 12:18:52 +0300

    dif classes

--- src/my/test/Main.java
+++ src/my/test/Main.java
@@ -8,6 +8,10 @@ public class Main {
                        {0, 0, 1, 1, 0, 0},
                        {0, 0, 0, 1, 0, 0}};
 
+//        int mat[][] = {{1, 0, 1, 1, 1},
+//                       {0, 0, 0, 0, 0},
+//                       {0, 0, 0, 0, 0}};
+
         int maxRow = 0;
         int maxColumn = 0;
         int row = 0, column=0;
@@ -19,19 +23,23 @@ public class Main {
                 if (mat[i][j] == 1) {
                     maxRow += 1;
                 }
+                else {
+                    maxRow = 0;
+                }
                 if (maxRow > resRow) {
                     resRow = maxRow;
                     row = i;
                 }
             }
-
         }
-        System.out.println("ROUND");
         for (int i = 0; i < 6; i++) {
             maxColumn = 0;
             for (int j = 0; j < 3; j++) {
                 if (mat[j][i] == 1) {
                     maxColumn += 1;
+                }
+                else{
+                    maxColumn = 0;
                 }
                 if (maxColumn > resColumn) {
                     resColumn = maxColumn;
@@ -41,10 +49,12 @@ public class Main {
 
         }
 
+
+
         System.out.println("Maximum 1's is " + ((resColumn > resRow) ? resColumn:resRow));
 
-        System.out.println("Row, that has max 1's: " + row);
-        System.out.println("Column, that has max 1's: " + column);
+        System.out.println("Row, that has max 1's: " + (row+1));
+        System.out.println("Column, that has max 1's: " + (column+1));
 
     }
 }
